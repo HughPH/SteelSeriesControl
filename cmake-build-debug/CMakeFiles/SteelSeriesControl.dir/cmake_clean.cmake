50b86a219d51395e78fd8abec680190926ef7ab6
Hugh Phoenix-Hulme <hugh@webnet.org.uk>
2018-09-23 19:25:43 +0100

    Added these CMake files. Are they required??

---
+++ cmake-build-debug/CMakeFiles/SteelSeriesControl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SteelSeriesControl.dir/main.cpp.o"
+  "SteelSeriesControl.pdb"
+  "SteelSeriesControl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SteelSeriesControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
